4b4a2a460dc482590a082064a8f55083a5b95cbc
Litman, Mike <mlitman@s114d-12.cs.cuw.edu>
2016-07-11 08:55:35 -0500

    Initial Commit

---
+++ DiceRoller/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DiceRoller
+//
+//  Created by Litman, Mike on 7/11/16.
+//  Copyright © 2016 Litman, Michael. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
